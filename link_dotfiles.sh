326806ed8953cb8d14fb4b3382cfcba9e906ef27
Chris Black <chris@ckblack.org>
2017-05-04 16:13:37 -0400

    typo

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -3,7 +3,7 @@
 # Don't run this unless you're ready to overwrite a lot of configuration files!
 # Back up first, no warranties, etc etc
 
-if [[ $OSTYPE == darwin* ]]; then ./osx; done
+if [[ $OSTYPE == darwin* ]]; then ./osx; fi
 
 mkdir -p ~/.config/git .R .ssh
 
