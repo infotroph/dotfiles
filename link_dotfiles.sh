f15a9f38a5f08e187bdf65678a2e8d71cc0bb673
Chris Black <chris@ckblack.org>
2017-05-04 16:14:14 -0400

    ask before overwriting

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -7,14 +7,14 @@ if [[ $OSTYPE == darwin* ]]; then ./osx; fi
 
 mkdir -p ~/.config/git .R .ssh
 
-ln -s "$PWD"/config/git/attributes ~/.config/git/attributes
-ln -s "$PWD"/config/git/ignore ~/.config/git/ignore
-ln -s "$PWD"/gitconfig ~/.gitconfig
-ln -s "$PWD"/"Package Control.sublime-settings" ~/"Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
-ln -s "$PWD"/"Preferences.sublime-settings" ~/"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-ln -s "$PWD"/profile ~/.profile
-ln -s "$PWD"/R/Makevars ~/.R/Makevars
-ln -s "$PWD"/Rprofile ~/.Rprofile
-ln -s "$PWD"/ssh/config ~/.ssh/config
-ln -s "$PWD"/tmux.conf ~/.tmux.conf
-ln -s "$PWD"/zshrc ~/.zshrc
+ln -si "$PWD"/config/git/attributes ~/.config/git/attributes
+ln -si "$PWD"/config/git/ignore ~/.config/git/ignore
+ln -si "$PWD"/gitconfig ~/.gitconfig
+ln -si "$PWD"/"Package Control.sublime-settings" ~/"Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
+ln -si "$PWD"/"Preferences.sublime-settings" ~/"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+ln -si "$PWD"/profile ~/.profile
+ln -si "$PWD"/R/Makevars ~/.R/Makevars
+ln -si "$PWD"/Rprofile ~/.Rprofile
+ln -si "$PWD"/ssh/config ~/.ssh/config
+ln -si "$PWD"/tmux.conf ~/.tmux.conf
+ln -si "$PWD"/zshrc ~/.zshrc
